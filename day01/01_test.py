8dfcdde1fee41274ac5dff29cb188758395ca9c3
Jerry Gregoire <jerry@arcadecoffee.com>
2021-12-29 12:33:40 -0600

    More playing around

--- day01/01_test.py
+++ day01/01_test.py
@@ -1,25 +1,23 @@
 import day01.src as src
 
 def test_count_floors_part_1():
-    assert src.count_floors('(())')[-1] == 0
-    assert src.count_floors('()()')[-1] == 0
-    assert src.count_floors('(((')[-1] == 3
-    assert src.count_floors('(()(()(')[-1] == 3
-    assert src.count_floors('))(((((')[-1] == 3
-    assert src.count_floors('())')[-1] == -1
-    assert src.count_floors('))(')[-1] == -1
-    assert src.count_floors(')))')[-1] == -3
-    assert src.count_floors(')())())')[-1] == -3
+    assert src.count_floors('(())')[0] == 0
+    assert src.count_floors('()()')[0] == 0
+    assert src.count_floors('(((')[0] == 3
+    assert src.count_floors('(()(()(')[0] == 3
+    assert src.count_floors('))(((((')[0] == 3
+    assert src.count_floors('())')[0] == -1
+    assert src.count_floors('))(')[0] == -1
+    assert src.count_floors(')))')[0] == -3
+    assert src.count_floors(')())())')[0] == -3
 
 
 def test_count_floors_part_2():
-    assert src.count_floors(')').index(-1) + 1 == 1
-    assert src.count_floors('()())').index(-1) + 1 == 5
+    assert src.count_floors(')')[1] == 1
+    assert src.count_floors('()())')[1] == 5
 
 
-def test_part1():
-    assert src.part1(src.FULL_INPUT_FILE) == 232
-
-
-def test_part2():
-    assert src.part2(src.FULL_INPUT_FILE) == 1783
+def test_part1_and_part2():
+    part1, part2 = src.count_floors(src.load_data(src.FULL_INPUT_FILE))
+    assert part1 == 232
+    assert part2 == 1783
--- day01/src.py
+++ day01/src.py
@@ -12,12 +12,13 @@ DEFAULT_INPUT_FILE = FULL_INPUT_FILE
 
 
 def count_floors(directions: str) -> List[int]:
-    floors = []
+    basement = None
     current_floor = 0
-    for c in directions:
-        current_floor += 1 if c == '(' else -1
-        floors.append(current_floor)
-    return floors
+    for i in range(len(directions)):
+        current_floor += 1 if directions[i] == '(' else -1
+        if not basement and current_floor == -1:
+            basement = i + 1
+    return current_floor, basement
 
 
 def load_data(input_file: str) -> str:
@@ -25,14 +26,11 @@ def load_data(input_file: str) -> str:
         return f.readline().strip()
 
 
-def part1(input_file: str = DEFAULT_INPUT_FILE):
-    return count_floors(load_data(input_file))[-1]
-
-
-def part2(input_file: str = DEFAULT_INPUT_FILE):
-    return count_floors(load_data(input_file)).index(-1) + 1
+def part1_and_part2(input_file: str = DEFAULT_INPUT_FILE):
+    part1, part2 =  count_floors(load_data(input_file))
+    print(f'Part 1: {part1}')
+    print(f'Part 1: {part2}')
 
 
 if __name__ == '__main__':
-    print(f'Part 1: {part1()}')
-    print(f'Part 1: {part2()}')
+    part1_and_part2()
