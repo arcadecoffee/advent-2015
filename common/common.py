626ff6af87d698b3e615a5e4572b9eae46bf34c8
Jerry Gregoire <jerry@arcadecoffee.com>
2021-12-30 11:20:19 -0600

    cleanup

--- common/common.py
+++
@@ -1,6 +0,0 @@
-def print_progress_bar(total_steps: int = 100, current_step: int = 0):
-    if current_step == total_steps:
-        print(f'[{"=" * 20}] 100%')
-    else:
-        progress = int(20 * current_step / total_steps)
-        print(f'[{"=" * progress}>{"-" * (19 - progress)}] {progress * 5}%', end='\r')
