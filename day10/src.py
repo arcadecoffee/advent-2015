d31b7f47ba639134f47ce8c3573c3eb8b40663f5
Jerry Gregoire <jerry@arcadecoffee.com>
2022-01-03 17:33:46 -0600

    day 10 - part 1 & 2, brute force

--- day10/src.py
+++ day10/src.py
@@ -3,6 +3,8 @@ Advent of Code 2015 - Day 10
 https://adventofcode.com/2015/day/10
 """
 
+import re
+
 from pathlib import Path
 
 __mypath = Path(__file__).resolve().parent
@@ -16,14 +18,26 @@ def load_data(input_file: str) -> str:
         return f.readline().strip()
 
 
-def part1(input_file: str = DEFAULT_INPUT_FILE):
-    data = load_data(input_file)
-    return -1
+def look_and_say(look: str, cycles: int = 5):
+    for _ in range(cycles):
+        say = ''
+        offset = 0
+        while offset < len(look):
+            print(f'\r{_}: {len(look) - offset}', end='')
+            end = re.match(r'^(.)\1*', look[offset:]).end()
+            say += f'{end}{look[offset]}'
+            offset += end
+        look = say
+    print('\r', end='')
+    return look
 
 
-def part2(input_file: str = DEFAULT_INPUT_FILE):
-    data = load_data(input_file)
-    return -1
+def part1(input_file: str = DEFAULT_INPUT_FILE) -> int:
+    return len(look_and_say(load_data(input_file), 40))
+
+
+def part2(input_file: str = DEFAULT_INPUT_FILE) -> int:
+    return len(look_and_say(load_data(input_file), 50))
 
 
 if __name__ == '__main__':
--- day10/src_test.py
+++ day10/src_test.py
@@ -2,16 +2,12 @@ import day10.src as src
 
 
 def test_part1():
-    assert src.part1(src.TEST_INPUT_FILE) == 0
+    assert len(src.look_and_say(src.load_data(src.TEST_INPUT_FILE), 5)) == 6
 
 
 def test_part1_full():
-    assert src.part1(src.FULL_INPUT_FILE) == 0
-
-
-def test_part2():
-    assert src.part2(src.TEST_INPUT_FILE) == 0
+    assert src.part1(src.FULL_INPUT_FILE) == 492982
 
 
 def test_part2_full():
-    assert src.part2(src.FULL_INPUT_FILE) == 0
+    assert src.part2(src.FULL_INPUT_FILE) == 6989950
