01d22265f325d8a08c67b6066629772e28215e73
Jerry Gregoire <jerry@arcadecoffee.com>
2022-01-03 16:24:43 -0600

    day 9 - part 1 & 2

--- day09/src.py
+++ day09/src.py
@@ -3,7 +3,11 @@ Advent of Code 2015 - Day 9
 https://adventofcode.com/2015/day/9
 """
 
+import re
+
+from collections import defaultdict
 from pathlib import Path
+from typing import Dict, List
 
 __mypath = Path(__file__).resolve().parent
 FULL_INPUT_FILE = __mypath / 'input.full.txt'
@@ -11,19 +15,28 @@ TEST_INPUT_FILE = __mypath / 'input.test.txt'
 DEFAULT_INPUT_FILE = FULL_INPUT_FILE
 
 
-def load_data(input_file: str) -> str:
+def load_routes(input_file: str) -> Dict[str, Dict[str, int]]:
+    routes = defaultdict(dict)
     with open(input_file) as f:
-        return f.readline().strip()
+        for line in f:
+            a, b, distance = re.match(r'(\w+) to (\w+) = (\d+)', line).groups()
+            routes[a][b] = routes[b][a] = int(distance)
+    return routes
+
+
+def find_route(routes: Dict, visited: List = None, cost: int = 0, mode: callable = min) -> int:
+    return mode([find_route(routes, [origin], mode=mode) for origin in routes]) if not visited \
+        else mode([find_route(routes, visited + [destination],
+                              cost + routes[visited[-1]][destination], mode=mode)
+                   for destination in routes[visited[-1]] if destination not in visited] or [cost])
 
 
 def part1(input_file: str = DEFAULT_INPUT_FILE):
-    data = load_data(input_file)
-    return 0
+    return find_route(load_routes(input_file))
 
 
 def part2(input_file: str = DEFAULT_INPUT_FILE):
-    data = load_data(input_file)
-    return 0
+    return find_route(load_routes(input_file), mode=max)
 
 
 if __name__ == '__main__':
--- day09/src_test.py
+++ day09/src_test.py
@@ -2,16 +2,16 @@ import day09.src as src
 
 
 def test_part1():
-    assert src.part1(src.TEST_INPUT_FILE) == 0
+    assert src.part1(src.TEST_INPUT_FILE) == 605
 
 
 def test_part1_full():
-    assert src.part1(src.FULL_INPUT_FILE) == 0
+    assert src.part1(src.FULL_INPUT_FILE) == 251
 
 
 def test_part2():
-    assert src.part2(src.TEST_INPUT_FILE) == 0
+    assert src.part2(src.TEST_INPUT_FILE) == 982
 
 
 def test_part2_full():
-    assert src.part2(src.FULL_INPUT_FILE) == 0
+    assert src.part2(src.FULL_INPUT_FILE) == 898
