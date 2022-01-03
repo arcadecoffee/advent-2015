586c4f6d6b42262532e6fef64caa64851636e257
Jerry Gregoire <jerry@arcadecoffee.com>
2022-01-03 13:33:45 -0600

    day 8 - part 1 & 2

--- day08/src.py
+++ day08/src.py
@@ -3,27 +3,56 @@ Advent of Code 2015 - Day 8
 https://adventofcode.com/2015/day/8
 """
 
+import re
+
 from pathlib import Path
+from typing import List
 
 __mypath = Path(__file__).resolve().parent
 FULL_INPUT_FILE = __mypath / 'input.full.txt'
-TEST_INPUT_FILE = __mypath / 'input.test1.txt'
+TEST_INPUT_FILE = __mypath / 'input.test.txt'
 DEFAULT_INPUT_FILE = FULL_INPUT_FILE
 
 
-def load_data(input_file: str) -> str:
+def decode_string(s: str) -> str:
+    translations = {
+        r'\\\\': '\\',
+        r'\\"': '"',
+        r'\\x..': lambda l: chr(int(l.group(0)[2:], 16))
+    }
+    return re.compile('|'.join(translations.keys())).sub(
+        lambda l: translations[r'\\x..'](l) if l.group(0).startswith('\\x') else
+        translations[re.escape(l.group(0))], s[1:-1])
+
+
+def encode_string(s: str) -> str:
+    translations = {
+        r'\\': '\\\\',
+        r'"': '\\"',
+    }
+    return '"' + re.compile('|'.join(translations.keys())).sub(
+        lambda l: translations[re.escape(l.group(0))], s) + '"'
+
+
+def load_data(input_file: str) -> List[str]:
     with open(input_file) as f:
-        return f.readline().strip()
+        return [line.strip() for line in f]
 
 
-def part1(input_file: str = DEFAULT_INPUT_FILE):
-    data = load_data(input_file)
-    return 0
+def part1(input_file: str = DEFAULT_INPUT_FILE) -> int:
+    raw_size = decoded_size = 0
+    for s in load_data(input_file):
+        raw_size += len(s)
+        decoded_size += len(decode_string(s))
+    return raw_size - decoded_size
 
 
-def part2(input_file: str = DEFAULT_INPUT_FILE):
-    data = load_data(input_file)
-    return 0
+def part2(input_file: str = DEFAULT_INPUT_FILE) -> int:
+    raw_size = encoded_size = 0
+    for s in load_data(input_file):
+        raw_size += len(s)
+        encoded_size += len(encode_string(s))
+    return encoded_size - raw_size
 
 
 if __name__ == '__main__':
--- day08/src_test.py
+++ day08/src_test.py
@@ -2,16 +2,16 @@ import day08.src as src
 
 
 def test_part1():
-    assert src.part1(src.TEST_INPUT_FILE) == 0
+    assert src.part1(src.TEST_INPUT_FILE) == 12
 
 
 def test_part1_full():
-    assert src.part1(src.FULL_INPUT_FILE) == 0
+    assert src.part1(src.FULL_INPUT_FILE) == 1350
 
 
 def test_part2():
-    assert src.part2(src.TEST_INPUT_FILE) == 0
+    assert src.part2(src.TEST_INPUT_FILE) == 19
 
 
 def test_part2_full():
-    assert src.part2(src.FULL_INPUT_FILE) == 0
+    assert src.part2(src.FULL_INPUT_FILE) == 2085
