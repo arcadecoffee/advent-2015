f7e11639e8b26b464d782a0ed259bc48c60375db
Jerry Gregoire <jerry@arcadecoffee.com>
2022-01-05 12:13:13 -0600

    day 14 - part 1 & 2

--- day14/src.py
+++ day14/src.py
@@ -3,7 +3,11 @@ Advent of Code 2015 - Day 14
 https://adventofcode.com/2015/day/14
 """
 
+import re
+
+from collections import defaultdict
 from pathlib import Path
+from typing import Any, Generator, List, Union, Dict
 
 __mypath = Path(__file__).resolve().parent
 FULL_INPUT_FILE = __mypath / 'input.full.txt'
@@ -11,19 +15,43 @@ TEST_INPUT_FILE = __mypath / 'input.test.txt'
 DEFAULT_INPUT_FILE = FULL_INPUT_FILE
 
 
-def load_data(input_file: str) -> str:
+def load_data(input_file: str) -> Dict[str, Dict[str, int]]:
+    data = {}
     with open(input_file) as f:
-        return f.readline().strip()
+        for line in f:
+            name, speed, duration, rest = \
+                (int(i) if i.isdigit() else i
+                 for i in re.match(r'(\w+).* (\d+).* (\d+).* (\d+)', line).groups())
+            data[name] = {'speed': speed, 'duration': duration, 'rest': rest}
+    return data
 
 
-def part1(input_file: str = DEFAULT_INPUT_FILE):
-    data = load_data(input_file)
-    return 0
+def run_race(reindeer: Dict[str, Dict[str, int]], race_duration: int) -> Dict[str, int]:
+    results = {}
+    for k, v in reindeer.items():
+        cycle = v['duration'] + v['rest']
+        run_seconds = \
+            (int(race_duration / cycle) * v['duration']) + min([v['duration'], race_duration % cycle])
+        results[k] = v['speed'] * ((int(race_duration / cycle) * v['duration']) +
+                                   min([v['duration'], race_duration % cycle]))
+    return results
 
 
-def part2(input_file: str = DEFAULT_INPUT_FILE):
-    data = load_data(input_file)
-    return 0
+def score_race(reindeer: Dict[str, Dict[str, int]], race_duration: int) -> Dict[str, int]:
+    results = defaultdict(int)
+    for s in range(race_duration):
+        split_results = run_race(reindeer, s + 1)
+        for r in [r for r in split_results if split_results[r] == max(split_results.values())]:
+            results[r] += 1
+    return results
+
+
+def part1(input_file: str = DEFAULT_INPUT_FILE, race_duration: int = 2503):
+    return max(run_race(load_data(input_file), race_duration).values())
+
+
+def part2(input_file: str = DEFAULT_INPUT_FILE, race_duration: int = 2503):
+    return max(score_race(load_data(input_file), race_duration).values())
 
 
 if __name__ == '__main__':
--- day14/src_test.py
+++ day14/src_test.py
@@ -1,17 +1,29 @@
 import day14.src as src
 
 
+def test_run_race():
+    results = src.run_race(src.load_data(src.TEST_INPUT_FILE), 1000)
+    assert results['Comet'] == 1120
+    assert results['Dancer'] == 1056
+
+
 def test_part1():
-    assert src.part1(src.TEST_INPUT_FILE) == 1120
+    assert src.part1(src.TEST_INPUT_FILE, 1000) == 1120
 
 
 def test_part1_full():
-    assert src.part1(src.FULL_INPUT_FILE) == 1
+    assert src.part1(src.FULL_INPUT_FILE) == 2696
+
+
+def test_score_race():
+    results = src.score_race(src.load_data(src.TEST_INPUT_FILE), 1000)
+    assert results['Comet'] == 312
+    assert results['Dancer'] == 689
 
 
 def test_part2():
-    assert src.part2(src.TEST_INPUT_FILE) == 1
+    assert src.part2(src.TEST_INPUT_FILE, 1000) == 689
 
 
 def test_part2_full():
-    assert src.part2(src.FULL_INPUT_FILE) == 1
+    assert src.part2(src.FULL_INPUT_FILE) == 1084
