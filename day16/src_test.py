101c62a442de4dd0a8de23772852422cc8be783f
Jerry Gregoire <jerry@arcadecoffee.com>
2022-01-05 16:50:58 -0600

    day 16 - templates and input

---
+++ day16/src_test.py
@@ -0,0 +1,17 @@
+import day16.src as src
+
+
+def test_part1():
+    assert src.part1(src.TEST_INPUT_FILE) == 1
+
+
+def test_part1_full():
+    assert src.part1(src.FULL_INPUT_FILE) == 1
+
+
+def test_part2():
+    assert src.part2(src.TEST_INPUT_FILE) == 1
+
+
+def test_part2_full():
+    assert src.part2(src.FULL_INPUT_FILE) == 1
