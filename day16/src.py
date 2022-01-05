ebadee63563851432299ff0c295d515d89093c8d
Jerry Gregoire <jerry@arcadecoffee.com>
2022-01-05 17:23:49 -0600

    day 16 - almost certainly a more elegant way to handle the rules but fatigues is setting in

--- day16/src.py
+++ day16/src.py
@@ -11,19 +11,70 @@ TEST_INPUT_FILE = __mypath / 'input.test.txt'
 DEFAULT_INPUT_FILE = FULL_INPUT_FILE
 
 
-def load_data(input_file: str) -> str:
+def load_data(input_file: str) -> dict:
     with open(input_file) as f:
-        return f.readline().strip()
+        return {
+            s[:s.index(':')]: dict([(t.split(': ')[0], int(t.split(': ')[1]))
+                                    for t in s[s.index(':') + 2:].split(', ')])
+            for s in f
+        }
+
+
+def find_sue(data: dict) -> int:
+    knowns = {
+        'children': 3,
+        'cats': 7,
+        'samoyeds': 2,
+        'pomeranians': 3,
+        'akitas': 0,
+        'vizslas': 0,
+        'goldfish': 5,
+        'trees': 3,
+        'cars': 2,
+        'perfumes': 1,
+    }
+
+    for fact, value in knowns.items():
+        data = {k: v for k, v in data.items() if v.get(fact, value) == value}
+
+    return int(list(data.keys())[0].split(' ')[1])
+
+
+def find_real_sue(data: dict) -> int:
+    knowns = {
+        'children': 3,
+        'samoyeds': 2,
+        'akitas': 0,
+        'vizslas': 0,
+        'cars': 2,
+        'perfumes': 1,
+    }
+    for fact, value in knowns.items():
+        data = {k: v for k, v in data.items() if v.get(fact, value) == value}
+
+    greater_thans = {
+        'cats': 7,
+        'trees': 3,
+    }
+    for fact, value in greater_thans.items():
+        data = {k: v for k, v in data.items() if v.get(fact, value + 1) > value}
+
+    less_thans = {
+        'pomeranians': 3,
+        'goldfish': 5,
+    }
+    for fact, value in less_thans.items():
+        data = {k: v for k, v in data.items() if v.get(fact, value - 1) < value}
+
+    return int(list(data.keys())[0].split(' ')[1])
 
 
 def part1(input_file: str = DEFAULT_INPUT_FILE):
-    data = load_data(input_file)
-    return 0
+    return find_sue(load_data(input_file))
 
 
 def part2(input_file: str = DEFAULT_INPUT_FILE):
-    data = load_data(input_file)
-    return 0
+    return find_real_sue(load_data(input_file))
 
 
 if __name__ == '__main__':
--- day16/src_test.py
+++ day16/src_test.py
@@ -1,17 +1,9 @@
 import day16.src as src
 
 
-def test_part1():
-    assert src.part1(src.TEST_INPUT_FILE) == 1
-
-
 def test_part1_full():
-    assert src.part1(src.FULL_INPUT_FILE) == 1
-
-
-def test_part2():
-    assert src.part2(src.TEST_INPUT_FILE) == 1
+    assert src.part1(src.FULL_INPUT_FILE) == 103
 
 
 def test_part2_full():
-    assert src.part2(src.FULL_INPUT_FILE) == 1
+    assert src.part2(src.FULL_INPUT_FILE) == 405
