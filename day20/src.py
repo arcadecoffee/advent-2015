407493ad25927c64642e3e7c0ca98d452bf417e1
Jerry Gregoire <jerry@arcadecoffee.com>
2022-01-08 13:50:18 -0600

    Day 20 - Part 1 & 2

--- day20/src.py
+++ day20/src.py
@@ -7,8 +7,6 @@ import itertools
 import math
 import pathlib
 
-from typing import Set
-
 __mypath = pathlib.Path(__file__).resolve().parent
 FULL_INPUT_FILE = __mypath / 'input.full.txt'
 TEST_INPUT_FILE = __mypath / 'input.test.txt'
@@ -20,21 +18,14 @@ def load_data(input_file: str) -> int:
         return int(f.readline().strip())
 
 
-def get_gift_count(house_number: int, gift_factor: int = 10) -> int:
+def get_gift_count(house_number: int, gift_factor: int = 10, max_stops: int = None) -> int:
     divisor_sum = 0
-    for i in range(1, math.ceil(math.sqrt(house_number))):
+    max_stops = math.ceil(math.sqrt(house_number)) if not max_stops else max_stops
+    for i in range(1, min(max_stops, int(math.sqrt(house_number))) + 1):
         if house_number % i == 0:
-            divisor_sum += i + (house_number / i)
-    divisor_sum += math.sqrt(house_number) if math.sqrt(house_number) % 1 else 0
-    return divisor_sum * gift_factor
-
-
-def get_gift_count_2(house_number: int, gift_factor: int = 11) -> int:
-    divisor_sum = 0
-    for i in range(1, math.ceil(math.sqrt(house_number))):
-        if house_number % i == 0:
-            divisor_sum += (house_number / i) + (i if house_number / i <= 50 else 0)
-    divisor_sum += math.sqrt(house_number) if math.sqrt(house_number) % 1 else 0
+            divisor_sum += house_number / i
+            divisor_sum += i if house_number / i <= 50 else 0
+    divisor_sum += math.sqrt(house_number) if math.sqrt(house_number) % 1 == 0 else 0
     return divisor_sum * gift_factor
 
 
@@ -48,7 +39,7 @@ def part1(input_file: str = DEFAULT_INPUT_FILE):
 def part2(input_file: str = DEFAULT_INPUT_FILE):
     target = load_data(input_file)
     for i in itertools.count(1):
-        if get_gift_count_2(i) >= target:
+        if get_gift_count(i, gift_factor=11, max_stops=50) >= target:
             return i
 
 
