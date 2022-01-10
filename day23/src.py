ff4fb864965c0bd428346679aea67b4f5a128193
Jerry Gregoire <jerry@arcadecoffee.com>
2022-01-10 12:24:44 -0600

    Day 23 - Part 1 & 2

--- day23/src.py
+++ day23/src.py
@@ -4,6 +4,9 @@ https://adventofcode.com/2015/day/23
 """
 
 import pathlib
+import re
+
+from typing import Any, Dict, List
 
 __mypath = pathlib.Path(__file__).resolve().parent
 FULL_INPUT_FILE = __mypath / 'input.full.txt'
@@ -11,19 +14,48 @@ TEST_INPUT_FILE = __mypath / 'input.test.txt'
 DEFAULT_INPUT_FILE = FULL_INPUT_FILE
 
 
-def load_data(input_file: str) -> str:
+class Computer:
+    def __init__(self, a: int = 0, b: int = 0):
+        self.registers: Dict[str, int] = {'a': a, 'b': b}
+
+    def execute_instructions(self, instructions: List[List[Any]]):
+        instruction_pointer: int = 0
+        while instruction_pointer < len(instructions):
+            curr = instructions[instruction_pointer]
+            if curr[0] == 'jmp':
+                instruction_pointer += curr[1]
+            elif ((curr[0] == 'jio' and self.registers[curr[1]] == 1)
+                  or (curr[0] == 'jie' and not (self.registers[curr[1]] % 2))):
+                instruction_pointer += curr[2]
+            else:
+                if curr[0] == 'hlf':
+                    self.registers[curr[1]] = int(self.registers[curr[1]] / 2)
+                elif curr[0] == 'tpl':
+                    self.registers[curr[1]] = self.registers[curr[1]] * 3
+                elif curr[0] == 'inc':
+                    self.registers[curr[1]] = self.registers[curr[1]] + 1
+                instruction_pointer += 1
+
+
+def load_data(input_file: str) -> List[List[Any]]:
     with open(input_file) as f:
-        return f.readline().strip()
+        data = [l.strip() for l in f]
+        data = [re.split(' |, ', d) for d in data]
+        return [[int(f) if not f.isalpha() else f for f in d] for d in data]
 
 
 def part1(input_file: str = DEFAULT_INPUT_FILE) -> int:
     data = load_data(input_file)
-    return -1
+    c = Computer()
+    c.execute_instructions(data)
+    return c.registers['b']
 
 
 def part2(input_file: str = DEFAULT_INPUT_FILE) -> int:
     data = load_data(input_file)
-    return -1
+    c = Computer(a=1)
+    c.execute_instructions(data)
+    return c.registers['b']
 
 
 if __name__ == '__main__':
--- day23/src_test.py
+++ day23/src_test.py
@@ -1,17 +1,9 @@
 import day23.src as src
 
 
-def test_part1():
-    assert src.part1(src.TEST_INPUT_FILE) == 0
-
-
 def test_part1_full():
-    assert src.part1(src.FULL_INPUT_FILE) == 0
-
-
-def test_part2():
-    assert src.part2(src.TEST_INPUT_FILE) == 0
+    assert src.part1(src.FULL_INPUT_FILE) == 184
 
 
 def test_part2_full():
-    assert src.part2(src.FULL_INPUT_FILE) == 0
+    assert src.part2(src.FULL_INPUT_FILE) == 231
